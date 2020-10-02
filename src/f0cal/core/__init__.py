321ef49229c9fc25da7651d25c8d723ea719c739
quincy2112 <qphiwum@gmail.com>
2020-10-01 23:46:28 -0400

    combined contents of f0cal/__init__.py and f0cal/core/__init__.py
    Commented out contents of f0cal/__init__.py

--- src/f0cal/core/__init__.py
+++ src/f0cal/core/__init__.py
@@ -1,5 +1,10 @@
 # -*- coding: utf-8 -*-
 from pkg_resources import get_distribution, DistributionNotFound
+__import__("pkg_resources").declare_namespace(__name__)
+
+import wrapt
+
+from src.f0cal.core.state import StateManager
 
 try:
     # Change here if project is renamed and does not equal the package name
@@ -9,3 +14,28 @@ except DistributionNotFound:
     __version__ = 'unknown'
 finally:
     del get_distribution, DistributionNotFound
+
+
+CORE = StateManager()
+plugin = CORE.scanner.make_plugin_decorator
+entrypoint = CORE.cli.entrypoint
+
+@wrapt.decorator
+def api_entrypoint(fn, _, args, dargs):
+    CORE.scanner.scan("f0cal")
+    return fn(CORE, *args, **dargs)
+
+
+@plugin(name="f0cal", sets="config_file")
+def config():
+    prefix = CORE.prefix
+    return (
+        f"""
+    [f0cal]
+    prefix={prefix}
+    """
+        + """
+    [env]
+    LD_LIBRARY_PATH=${f0cal:prefix}/lib
+    """
+    )
