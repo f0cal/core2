a0f79c79019b4428146ff8d09414cce85b10f8d4
quincy2112 <qphiwum@gmail.com>
2020-10-05 20:52:47 -0400

    Updated cli.py

--- src/f0cal/core/cli.py
+++ src/f0cal/core/cli.py
@@ -19,7 +19,7 @@ def _env_args(parser):
         "-s", "--silent", dest="silent", default=False, action="store_true"
     )
 
-@f0cal.entrypoint(["env", "activate"], args=_env_args)
+@f0cal.core.entrypoint(["env", "activate"], args=_env_args)
 def _env_activate(parser, core, append, silent, **_):
     env_str = core.env_activate_str
     if silent and not append:
@@ -29,10 +29,10 @@ def _env_activate(parser, core, append, silent, **_):
     if append:
         core.append_to_venv_activate()
 
-@f0cal.entrypoint(["env", "deactivate"])
+@f0cal.core.entrypoint(["env", "deactivate"])
 def _env_deactivate(parser, core):
     raise NotImplementedError()
 
-@f0cal.entrypoint(["ini"])
+@f0cal.core.entrypoint(["ini"])
 def _ini(parser, core):
     core.run_all_ini()
